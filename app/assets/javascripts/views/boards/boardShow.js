6ca66d5fabf20a68d63e6482033cdfe9edbab2c6
acatxnamedvirtue <tsb252@gmail.com>
2015-09-14 17:48:16 -0400

    Implement dragging and dropping with saving positions.

--- app/assets/javascripts/views/boards/boardShow.js
+++ app/assets/javascripts/views/boards/boardShow.js
@@ -3,7 +3,9 @@ TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
   events: {
     'click .delete-list' : 'deleteList',
     'click .delete-card' : 'deleteCard',
-    'click .delete-board': 'deleteBoard'
+    'click .delete-board': 'deleteBoard',
+    'sortbeforestop .lists' : 'updateOrder',
+    'sortbeforestop .cards' : 'updateOrder'
   },
 
   initialize: function (options) {
@@ -14,6 +16,7 @@ TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
   render: function () {
     var content = this.template({ board: this.model });
     this.$el.html(content);
+
     return this;
   },
 
@@ -36,5 +39,34 @@ TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
   deleteBoard: function (e) {
     this.model.destroy();
     Backbone.history.navigate("", {trigger: true});
+  },
+
+  updateOrder: function (e) {
+    if (e.currentTarget.className.includes("list")) {
+      var lists = this.$('.list');
+
+      for(var i = 0; i < lists.length; i++) {
+        if(lists[i].className.includes("placeholder")){ continue }
+        var ord = $(lists[i]).data('ord');
+        if (i + 1 != ord) {
+          var listId = $(lists[i]).data('id');
+          this.model.lists().get(listId).save({ord: i + 1})
+        }
+      }
+    } else {
+      var listId = $(e.currentTarget).data('list-id');
+      var cards = $(e.currentTarget).find('.card');
+
+      for(var i = 0; i < cards.length; i++) {
+        if(cards[i].className.includes("placeholder")){ continue }
+        var ord = $(cards[i]).data('ord');
+        if (i + 1 != ord) {
+          var cardId = $(cards[i]).data('card-id');
+          this.model.lists().get(listId).cards().get(cardId).save({ord: i + 1})
+
+        }
+      }
+
+    }
   }
 })
